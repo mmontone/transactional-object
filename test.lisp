05dab2b06c2061fc5393fffe2c76e5e334509928
Mariano Montone <mariano@copyleft.no>
2011-04-02 00:25:31 -0300

    Tests to write

--- test.lisp
+++ test.lisp
@@ -6,5 +6,22 @@
    (last-name :initarg :last-name
 	      :accessor last-name))
   (:metaclass transactional-class))
-  
+
+;; Transaction interleaving
+
+;; (let ((t1 (make-instance 'transaction))
+;;       (t2 (make-instance 'transaction)))
+;;   (let ((*transaction* t1))
+;;     (setf (first-name *p*) "Mar"))
+;;   (let ((*transaction* t2))
+;;     (setf (first-name *p*) "Ar"))
+;;   (commit-transaction t1)
+;;   (commit-transaction t2))
+
+
+;; unwinding
+
+;; (block nil
+;;   (with-transaction ()
+;;     (setf (last-name *p*) "Martina") (return-from nil)))
 
